4474b76618373909d30359e1e6d15f7635264417
Kevin Huang <kevinhuang@Kevins-MacBook-Pro.local>
2025-03-25 16:21:10 -0400

    Category table include GoalIds

--- models/Category.js
+++ models/Category.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 const CategorySchema = new mongoose.Schema(
     {
         name:{type:String,required:true},
-        goalList:[]
+        goalList:{
+            type:[String]
+        }
     },
     {timestamps:true}
 )
--- routes/goal.js
+++ routes/goal.js
@@ -1,6 +1,16 @@
 const Goal = require('../models/Goal')
 const router = require('express').Router()
 
+
+router.get("/:id", async (req,res) => {
+    try{
+        const goal = await Goal.findById(req.params.id)
+        res.status(200).json(goal)
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
 router.post("/", async (req,res) => {
     const newGoal = new Goal(req.body)
     try{
